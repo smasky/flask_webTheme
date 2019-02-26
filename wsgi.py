40cac12172c27b0b2b6a0ab3cf6d8a06269baacc
smasky <492109831@qq.com>
2019-02-26 09:07:44 +0800

    '190226'

--- wsgi.py
+++ wsgi.py
@@ -4,6 +4,6 @@ import os
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 
 
-from SmaBlog import create_app  # noqa
+from Smablog import create_app  # noqa
 
-app = create_app('production')
+app = create_app('testing')
