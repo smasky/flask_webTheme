76f03f47159a207f2957d32839c0462c80039635
smasky <492109831@qq.com>
2019-03-18 15:59:08 +0800

    'l'

--- wsgi.py
+++ wsgi.py
@@ -5,5 +5,11 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 
 
 from SmaBlog import create_app  # noqa
+from flask_script import Manager
 
 app = create_app('production')
+
+manager = Manager(app)  
+  
+if __name__ == '__main__':  
+    manager.run() 
