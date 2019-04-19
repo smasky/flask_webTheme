da6b970bc80ed66a1cd5ed73dec706b987946bc5
smasky <492109831@qq.com>
2019-04-19 16:33:53 +0800

    'addPostMusic'

--- SmaBlog/models.py
+++ SmaBlog/models.py
@@ -7,6 +7,7 @@ class Post(db.Model):
     title=db.Column(db.String(60))
     post_img=db.Column(db.String(60))
     abstract=db.Column(db.String(60))
+    url=db.Column(db.String(60))
     body=db.Column(db.Text)
     views=db.Column(db.Integer)
     timestamp=db.Column(db.DateTime,default=datetime.utcnow,index=True)
--- SmaBlog/static/js/application.js
+++ SmaBlog/static/js/application.js
@@ -4,30 +4,49 @@ $(function() {
     $('a[data-pjax]').pjax();
 })
 
-function find_music(id,name,work,cover){
-    var baseUrl = 'http://120.79.36.48/';
-        //id = '28815250';
-        var url1 = baseUrl + 'music/url?id=' + id;
-        var url3 = baseUrl + 'lyric?id=' + id;
-        $.getJSON(url1, function (data1) {
-            var url_m = data1.url;
-            $.getJSON(url3, function (data) {
-                var lrc1 = data.lrc.lyric;
-                ap.list.add([{
-                    name: String(name),
-                    artist: String(work),
-                    url: url_m,
-                    cover: String(cover),
-                    lrc: lrc1,
-                }]);
-
-            });
-        });
+function playPostMusic(id) {
+    const subap = new APlayer({
+        container: document.getElementById('sub-aplayer'),
+        autoplay: false,
+        audio: []
+    });
+    var url = 'https://api.itooi.cn/music/netease/song?key=579621905&id=' + id
+    $.post(url, function(data1) {
+        console.log(data1.data);
+        subap.list.add([{
+            name: data1.data.name,
+            artist: data1.data.singer,
+            url: data1.data.url,
+            cover: data1.data.pic,
+        }]);
+    }, 'json');
+    subap.play();
 }
 
-function find_playlist(id){
+function find_music(id, name, work, cover) {
     var baseUrl = 'http://120.79.36.48/';
-    url1=baseUrl + 'playlist/detail?id=' + id;
+    //id = '28815250';
+    var url1 = baseUrl + 'music/url?id=' + id;
+    var url3 = baseUrl + 'lyric?id=' + id;
+    $.getJSON(url1, function(data1) {
+        var url_m = data1.url;
+        $.getJSON(url3, function(data) {
+            var lrc1 = data.lrc.lyric;
+            ap.list.add([{
+                name: String(name),
+                artist: String(work),
+                url: url_m,
+                cover: String(cover),
+                lrc: lrc1,
+            }]);
+
+        });
+    });
+}
+
+function find_playlist(id) {
+    var baseUrl = 'http://120.79.36.48/';
+    url1 = baseUrl + 'playlist/detail?id=' + id;
     $.ajax({
         url: baseUrl + 'playlist/detail?id=' + id,
         beforeSend: () => {
@@ -36,8 +55,8 @@ function find_playlist(id){
         success: (data) => {
             console.log('歌单拉取成功！');
             var music = data;
-            for (var i=0;i<music.length;i++){
-                find_music(music[i].song_id,music[i].name,music[i].author,music[i].cover)
+            for (var i = 0; i < music.length; i++) {
+                find_music(music[i].song_id, music[i].name, music[i].author, music[i].cover)
             }
         },
         fail: (status) => {
@@ -45,12 +64,12 @@ function find_playlist(id){
         }
     });
 
-
 }
+
 function isMobile() {
     var userAgentInfo = navigator.userAgent;
 
-    var mobileAgents = [ "Android", "iPhone", "SymbianOS", "Windows Phone","iPod"];
+    var mobileAgents = ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPod"];
 
     var mobile_flag = false;
 
@@ -62,15 +81,15 @@ function isMobile() {
         }
     }
 
-     var screen_width = window.screen.width;
-     var screen_height = window.screen.height;
+    var screen_width = window.screen.width;
+    var screen_height = window.screen.height;
 
-     //根据屏幕分辨率判断是否是手机
-     if(screen_width < 500 && screen_height < 800){
-         mobile_flag = true;
-     }
+    //根据屏幕分辨率判断是否是手机
+    if (screen_width < 500 && screen_height < 800) {
+        mobile_flag = true;
+    }
 
-     return mobile_flag;
+    return mobile_flag;
 }
 
 function GenerateContentList() {
@@ -140,8 +159,4 @@ function GenerateContentList() {
             }
         }
     }
-}
-
-function fix_div(){
-
 }
